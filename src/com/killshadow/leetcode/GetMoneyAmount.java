c83e61999db0dd91c64bc94083b3e70bee8c64f6
killshadow <chaceli@foxmail.com>
2020-03-06 07:51:55 +0800

    GetMoneyAmount

--- src/com/killshadow/leetcode/GetMoneyAmount.java
+++ src/com/killshadow/leetcode/GetMoneyAmount.java
@@ -2,19 +2,19 @@ package com.killshadow.leetcode;
 
 public class GetMoneyAmount {
     public static void main(String[] args) {
-
+        System.out.println(new GetMoneyAmount().getMoneyAmount(10));
     }
 
     private int cost(int low, int high) {
-        if (low > high) {
+        if (low >= high) {
             return 0;
         }
-        int minres = Integer.MAX_VALUE;
-        for (int i = low; i < high; i++) {
-            int res = i + Math.max(cost(low, i), cost(i + 1, high));
-            minres = Math.min(minres, res);
+        int min = Integer.MAX_VALUE;
+        for (int i = (low + high) / 2; i <= high; i++) {
+            int res = i + Math.max(cost(low, i - 1), cost(i + 1, high));
+            min = Math.min(min, res);
         }
-        return minres;
+        return min;
     }
 
     public int getMoneyAmount(int n) {
