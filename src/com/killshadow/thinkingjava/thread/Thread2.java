727aeae7a8d8a1596ea9e7f79ae9b09dd0224d38
killshadow <chaceli@foxmail.com>
2020-07-20 23:37:12 +0800

    onjava8

--- src/com/killshadow/thinkingjava/thread/Thread2.java
+++ src/com/killshadow/thinkingjava/thread/Thread2.java
@@ -6,14 +6,32 @@ public class Thread2 {
         Thread t1 = new Thread(Thread2::print);
         Thread t2 = new Thread(Thread2::print);
 
+        Thread t3 = new Thread(Thread2::printSync);
+        Thread t4 = new Thread(Thread2::printSync);
+
         // Start both threads
-        t1.start();
-        t2.start();
+//        System.out.println("Start thread1.");
+//        t1.start();
+//        System.out.println("Start thread2.");
+//        t2.start();
+
+        t3.start();
+        t4.start();
     }
 
     public static void print() {
-        for (int i = 1; i <= 500; i++) {
+        System.out.println("Print start");
+        for (int i = 1; i <= 50; i++) {
             System.out.println(i);
         }
+        System.out.println("Print stop.");
+    }
+
+    public static synchronized void printSync() {
+        System.out.println("Print start");
+        for (int i = 1; i <= 50; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Print stop.");
     }
 }
