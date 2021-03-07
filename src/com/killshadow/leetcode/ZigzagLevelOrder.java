099a34325bd0b605a892192e63ab6bb36e5a6b04
killshadow <chaceli@foxmail.com>
2021-03-07 17:39:12 +0800

    onjava8

---
+++ src/com/killshadow/leetcode/ZigzagLevelOrder.java
@@ -0,0 +1,17 @@
+package com.killshadow.leetcode;
+
+import com.killshadow.utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZigzagLevelOrder {
+    public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        return res;
+    }
+}
--- src/com/killshadow/thinkingjava/thread/Thread2.java
+++ src/com/killshadow/thinkingjava/thread/Thread2.java
@@ -9,14 +9,22 @@ public class Thread2 {
         Thread t3 = new Thread(Thread2::printSync);
         Thread t4 = new Thread(Thread2::printSync);
 
-        // Start both threads
+        Thread t5 = new Thread(Thread2::printPartSync);
+        Thread t6 = new Thread(Thread2::printPartSync);
+
+        // 多线程竞争
 //        System.out.println("Start thread1.");
 //        t1.start();
 //        System.out.println("Start thread2.");
 //        t2.start();
 
-        t3.start();
-        t4.start();
+        // 多线程阻塞
+//        t3.start();
+//        t4.start();
+
+        // 多线程方法块阻塞
+        t5.start();
+        t6.start();
     }
 
     public static void print() {
@@ -31,6 +39,20 @@ public class Thread2 {
         System.out.println("Print start");
         for (int i = 1; i <= 50; i++) {
             System.out.println(i);
+        }
+        System.out.println("Print stop.");
+    }
+
+    // 可以一个实例方法和一个静态方法同步。构造函数不能声明为同步。
+    public static void printPartSync() {
+        System.out.println("Print start");
+        for (int i = 200; i <= 500; i++) {
+            System.out.println(i);
+        }
+        synchronized (Thread2.class) {
+            for (int i = 100; i <= 150; i++) {
+                System.out.println(i);
+            }
         }
         System.out.println("Print stop.");
     }
